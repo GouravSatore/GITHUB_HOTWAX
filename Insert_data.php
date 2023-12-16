028543c32276f31bd6ebfddbd9bc376947c348fb
Gourav Satore <76991367+GouravSatore@users.noreply.github.com>
2023-12-16 16:20:03 +0530

    Add files via upload

--- Insert_data.php
+++ Insert_data.php
@@ -6,14 +6,18 @@ $partyId = $_POST['partyId'];
 $salutation = $_POST['salutation'];
 $firstName = $_POST['firstName'];
 $middleName = $_POST['middleName'];
-$lastName = $_POST['gender'];
-$gender = $_POST['birthDate'];
+$lastName = $_POST['lastName'];
+$gender = $_POST['gender'];
+$birthDate = $_POST['birthDate'];
 $maritalStatusEnumId = $_POST['maritalStatusEnumId'];
 $employmentStatusEnumId = $_POST['employmentStatusEnumId'];
 $OCCUPATION = $_POST['OCCUPATION'];
 
-$query = "INSERT INTO `person` VALUES('$partyId','$salutation','$firstName','$middleName','$lastName','$gender','$maritalStatusEnumId','$employmentStatusEnumId','$OCCUPATION')";
-mysqli_query($conn, $query);
+$queryParty = "INSERT INTO `party` VALUES ('$partyId', 'PtyPerson')";
+mysqli_query($conn, $queryParty);
+
+$queryPerson = "INSERT INTO `person` VALUES('$partyId','$salutation','$firstName','$middleName','$lastName','$gender','$birthDate','$maritalStatusEnumId','$employmentStatusEnumId','$OCCUPATION')";
+mysqli_query($conn, $queryPerson);
 echo json_encode(array('response' => "Success"));
 mysqli_close($conn);
 
