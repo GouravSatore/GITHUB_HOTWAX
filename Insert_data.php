d6721d4a0919a4c1e5f95d366003f42a72be6d6a
Gourav Satore <76991367+GouravSatore@users.noreply.github.com>
2023-12-16 16:04:12 +0530

    insertion of data for person

--- Insert_data.php
+++ Insert_data.php
@@ -2,5 +2,19 @@
 $conn = mysqli_connect("localhost", "root", "");
 mysqli_select_db($conn, "new_hotwax");
 
+$partyId = $_POST['partyId'];
+$salutation = $_POST['salutation'];
+$firstName = $_POST['firstName'];
+$middleName = $_POST['middleName'];
+$lastName = $_POST['gender'];
+$gender = $_POST['birthDate'];
+$maritalStatusEnumId = $_POST['maritalStatusEnumId'];
+$employmentStatusEnumId = $_POST['employmentStatusEnumId'];
+$OCCUPATION = $_POST['OCCUPATION'];
+
+$query = "INSERT INTO `person` VALUES('$partyId','$salutation','$firstName','$middleName','$lastName','$gender','$maritalStatusEnumId','$employmentStatusEnumId','$OCCUPATION')";
+mysqli_query($conn, $query);
+echo json_encode(array('response' => "Success"));
+mysqli_close($conn);
 
 ?>
