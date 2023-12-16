d4c5d3af731c1650fd5a57777d9b643590e23061
Gourav Satore <76991367+GouravSatore@users.noreply.github.com>
2023-12-16 17:04:18 +0530

    Add files via upload

--- fetch_data_person.php
+++ fetch_data_person.php
@@ -2,7 +2,9 @@
 $conn = mysqli_connect("localhost", "root", "");
 mysqli_select_db($conn, "new_hotwax");
 
-$query = "SELECT * FROM `person`";
+$partyId = $_POST['partyId'];
+
+$query = "SELECT * FROM `person` Where `partyId` = '$partyId'";
 mysqli_query($conn,$query);
 echo json_encode(array('response' => "Success"));
 mysqli_close($conn);
